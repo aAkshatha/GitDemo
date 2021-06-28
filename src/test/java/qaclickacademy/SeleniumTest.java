b607196ebe949a125d3dabebf042240191acb21b
akshatha <akshatharai.1993@gmail.com>
2021-06-28 17:53:27 +0400

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void browseAutomation() {
+		System.out.println("browseAutomation");
+	}
+	
+	@Test
+	public void elementsUI() {
+		System.out.println("elementsUI");
+	}
+
+}
